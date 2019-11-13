3620bb19f3d35ef8bb5b2b9aa4f7aac4d9211e19
Barry <136592+logachu@users.noreply.github.com>
2019-11-13 00:38:49 -0800

    update comment

--- ManagerSpecialsPlayground.playground/Contents.swift
+++ ManagerSpecialsPlayground.playground/Contents.swift
@@ -9,9 +9,8 @@ import PlaygroundSupport
 // into  a framework so it can be shared between a playground and an
 // app project because its a lot quicker to iterate in a playground.
 //
-// Tip: Set the playground to "Manually Run" then define a keyboard
-//      shortcut to run it. Setting it to "Automatically Run" seems
-//      to eventually hang the playground.
+// Tip: Set the playground to "Manually Run". Setting it to "Automatically Run"
+//      seems to eventually hang the playground.
 let bundle = Bundle(for: ManagerSpecialsViewController.self)
 let storyboard = UIStoryboard(name: "Main", bundle: bundle)
 let vc = storyboard.instantiateInitialViewController() as! ManagerSpecialsViewController
