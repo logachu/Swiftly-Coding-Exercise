142118e578dfc06475ca6708dc03db28a0856d92
Barry <136592+logachu@users.noreply.github.com>
2019-11-14 21:03:18 -0800

    playground comments

--- ManagerSpecialsPlayground.playground/Contents.swift
+++ ManagerSpecialsPlayground.playground/Contents.swift
@@ -1,24 +1,31 @@
-//: A UIKit based Playground for presenting user interface
-  
+//: To speed up iterating on the view controller its packaged into a
+//: framework so it can be shared between a playground and an app
+//: project because its a lot quicker to iterate in a playground.
+//:
+//: Note: Updates to the framework must be re-built before the
+//: framework is re-run so select the ManagerSpecials scheme and hit
+//: ⌘R to build followed by ⇧⌘↩︎ to re-run the playground. It seems
+//: that sometimes the playground needs to be explicitly stopped
+//: before re-running. And sometimes the live view just stops
+//: appearing until you restart Xcode. “¯\_(ツ)_/¯“
+//:
+//: Tip: Set the playground to "Manually Run". Setting it to
+//: "Automatically Run" seems to eventually hang the playground.
+
 import UIKit
 import Combine
 import ManagerSpecials
 import PlaygroundSupport
 
-// To speed up iterating on a view controller its packaged
-// into  a framework so it can be shared between a playground and an
-// app project because its a lot quicker to iterate in a playground.
-//
-// Tip: Set the playground to "Manually Run". Setting it to "Automatically Run"
-//      seems to eventually hang the playground.
+
 let bundle = Bundle(for: ManagerSpecialsViewController.self)
 let storyboard = UIStoryboard(name: "Main", bundle: bundle)
 let vc = storyboard.instantiateInitialViewController() as! ManagerSpecialsViewController
-vc.preferredContentSize = CGSize(width: 414, height: 896) // iPHone 11 Xr
+vc.preferredContentSize = CGSize(width: 414, height: 896) // iPhone 11 Xr
 PlaygroundPage.current.liveView = vc
 
-vc.model = ManagerSpecialsModel()
-//vc.model = ManagerSpecialsModel(api: SwiftlyAPIMock())
+//vc.model = ManagerSpecialsModel()
+vc.model = ManagerSpecialsModel(api: SwiftlyAPIMock())
 
 func testAPIMock() {
     let api = SwiftlyAPIMock()
